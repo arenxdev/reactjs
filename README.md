3a1c61a7fa7e3e7153e4088b5609163bcf1c5c22
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-07-06 11:30:38 -0500

    Handle state

--- README.md
+++ README.md
@@ -152,3 +152,10 @@ Cuando hay un botón dentro de un formulario, este automáticamente será de tip
 
 1. Especificando que su valor es de tipo **button**.
 2. manejándolo desde el formulario cuando ocurre el evento **onSubmit**.
+
+### MANEJO DE ESTADOS
+
+Hasta esta clase todos los componentes han obtenido su información a través de **props** que vienen desde afuera (otros componentes) pero hay otra manera en la que los componentes pueden producir su propia información y guardarla para ser consumida o pasada a otros componentes a través de sus props. La clave está en que la información del **state** a otros componentes pasará en una sola dirección y podrá ser consumida pero no modificada.
+
+- Para guardar la información en el estado se usa una **función** de la clase component llamada `setState` a la cual se le debe pasar un objeto con la información que se quiere guardar.
+- Aunque no se ve, la información está siendo guardada en dos sitios. Cada input guarda su propio valor y al tiempo la está guardando en setState, lo cual no es ideal. Para solucionarlo hay que modificar los inputs de un estado de no controlados a controlados.
--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -3,24 +3,47 @@ import React, { Component } from 'react'
 class BadgeForm extends Component {
 
   handleChange = ({ target: { name, value} }) => {
-    console.log(value)
-    console.log(name)
+    this.setState({[name]: value})
   }
 
   handleClick = event => {
+    console.log('Button was clicked')
+  }
+  
+  handleSubmit = event => {
     event.preventDefault()
-    console.log('Button was click')
+    console.log('Form was submitted')
+    console.log(this.state)
   }
 
+  state = {firstName: '', lastName: '', email: '', jobTitle: '', twitter: ''}
   render() {
     return (
       <div>
         <h1>New Attendant</h1>
-        <form id="formbadge" className="form-group">
-          <label htmlFor="firstName">First Name</label>
-          <input onChange={this.handleChange} className="form-control" type="text" name="firstName" />
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="firstName">First Name</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="firstName" value={this.state.firstName} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastName">Last Name</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="lastName" value={this.state.lastName} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="email" value={this.state.email} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="jobTitle">Job Title</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="jobTitle" value={this.state.jobTitle} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="twitter">Twitter</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="twitter" value={this.state.twitter} />
+          </div>
+          <button type="submit" onClick={this.handleClick} className="btn btn-primary">Save</button>
         </form>
-        <button form="formbadge" onClick={this.handleClick} className="btn btn-primary">Save</button>
       </div>
     )
   }
